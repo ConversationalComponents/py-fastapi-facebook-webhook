3b8b3337b5563a8d22052ec810d70a7ccb87dbd8
Victor Vasiliev <victor@imperson.com>
2020-07-27 11:12:08 +0300

    Test optimizarion.

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -6,10 +6,15 @@ client = TestClient(app)
 
 
 def test_verification():
+    """
+    Test webhook verification mechanism.
+    """
+    challenge_value = "challenge"
+
     response = client.get("/api/webhook", params={
         "hub.mode": "subscribe",
         "hub.verify_token": "test",  ## Must match the VERIFY_TOKEN env variable.
-        "hub.challenge": "challenge"
+        "hub.challenge": challenge_value
     })
     assert response.status_code == 200
-    assert response.content == "challenge"
+    assert response.content == challenge_value
